5b19670baa75015aeaab52df6c2236826392e4ef
xidian <1287810069@qq.com>
2024-06-21 14:59:22 +0800

    create module

---
+++ simulation/src/optical/helper/optical-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef __OPTICAL_HELPER_H__
+#define __OPTICAL_HELPER_H__
+
+#include "ns3/optical.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* __OPTICAL_HELPER_H__ */
+
---
+++ simulation/src/optical/model/optical.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef __OPTICAL_H__
+#define __OPTICAL_H__
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* __OPTICAL_H__ */
+
